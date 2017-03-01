9df263427915a7faf60b20b43fe4fead1887cd42
Heisenberg <prabhat.ag@gmail.com>
2017-03-01 17:55:19 +0530

    added identifiers

--- lex.py
+++ lex.py
@@ -29,6 +29,7 @@ class Character:
 	def increment_mult(self, inc):
 		if (self.index + inc) >= len(string):
 			return -1
+		#print("IN incrmult self.index, index = ", self.index, inc)
 		self.index += inc
 		self.set_val(self.index)
 	
@@ -42,25 +43,66 @@ class Character:
 def is_space(char):
 	return (char == ' ' or char == '\n' or char == '\r' or char == '\t')
 
+def tokenize_and_forward(lexemeBegin, index, tok_type):
+	#print("in tokenize and forward for ", lexemeBegin.index, index)
+	token = string[lexemeBegin.index : index + 1]
+	print((tok_type, token))
+	#print("indices - ", lexemeBegin.index, index)
+	lexemeBegin.increment_mult(index - lexemeBegin.index)
+	#print("incremented indices - ", lexemeBegin.index, index)
+	
+def is_valid(lexemeBegin, index):
+	#print("in valid for  ", lexemeBegin.index, index)
+	#, " string = ", string[lexemeBegin.index:index])
+	
+	#if (not index is None):
+	#	print("index > lexeme ", index > lexemeBegin.index)
+	#else:
+	#	print("index = none")
+	
+	#print("ISVALID ? ", (not (index is None) and index > lexemeBegin.index))
+	return (not (index is None) and index > lexemeBegin.index)
+		
 def start():	
 	lexemeBegin = Character(0)
 	print("here ", lexemeBegin.val)
-	
+	index = -1
+	store = Character(0)
 	while lexemeBegin.index < len(string):
-		index = keyword(lexemeBegin.index)
-		if not index is None and index > lexemeBegin.index:
-				token = string[lexemeBegin.index : index + 1]
-				print(("KEYWORD", token))
-				lexemeBegin.increment_mult(index - lexemeBegin.index)
-				
+		#print("lexemeBegin.index ", lexemeBegin.index)
+		if is_valid(lexemeBegin, keyword(lexemeBegin.index, store)):
+			tokenize_and_forward(lexemeBegin, store.index, "KEYWORD")
+		elif is_valid(lexemeBegin, identifier(lexemeBegin.index, store)):
+			tokenize_and_forward(lexemeBegin, store.index,"IDENTIFIER")
 		else:
 			if lexemeBegin.index + 1 == len(string):
 				break;
 			lexemeBegin.increment()
 
+#prabhat code		
+def digit(c):
+	return c.isdigit()
+	
+def isLetter(c):
+	return c.isalpha()
+	
+def identifier(index, store):
+	ch = Character(index)
+	if(isLetter(ch.val)):
+		ch.increment()
+		while(digit(ch.val) or isLetter(ch.val) or ch.val == '_'):
+			ch.increment()
+		ch.decrement()
+		store.index = ch.index
+		return store.index
+		
+	else:
+		return -1 
+		
+
 keywords = ['break', 'case', 'char', 'continue', 'do', 'double', 'else', 'for', 'float', 'if', 'int', 'include', 'long', 'return', 'sizeof', 'static', 'switch', 'void', 'while']
 			
-def keyword(index):
+def keyword(index, store):
 	#print("in keyword - ", index, string[index])
 	ch = Character(index)
 	if (ch.val == 'b'):
@@ -75,7 +117,8 @@ def keyword(index):
 						ch.increment()
 						if (is_space(ch.val) or ch.val == ';'):
 							ch.decrement()
-							return ch.index
+							store.index = ch.index
+							return store.index
 	elif (ch.val == 'c'):		
 		ch.increment()
 		if (ch.val == 'a'):
@@ -86,7 +129,8 @@ def keyword(index):
 					ch.increment()
 					if (is_space(ch.val)):
 						ch.decrement()
-						return ch.index
+						store.index = ch.index
+						return store.index
 		elif (ch.val == 'h'):
 			ch.increment()
 			if (ch.val == 'a'):
@@ -95,7 +139,8 @@ def keyword(index):
 					ch.increment()
 					if (is_space(ch.val)):
 						ch.decrement()
-						return ch.index
+						store.index = ch.index
+						return store.index
 		elif (ch.val == 'o'):
 			ch.increment()
 			if (ch.val == 'n'):
@@ -112,14 +157,16 @@ def keyword(index):
 									ch.increment()
 									if (is_space(ch.val) or ch.val == ';'):
 										ch.decrement()
-										return ch.index
+										store.index = ch.index
+										return store.index
 	elif (ch.val == 'd'):
 		ch.increment()
 		if (ch.val == 'o'):
 			ch.increment()
 			if (is_space(ch.val) or ch.val == '{'):
 				ch.decrement()
-				return ch.index
+				store.index = ch.index
+				return store.index
 			elif (ch.val == 'u'):
 				ch.increment()
 				if (ch.val == 'b'):
@@ -130,7 +177,8 @@ def keyword(index):
 							ch.increment()
 							if (is_space(ch.val)):
 								ch.decrement()
-								return ch.index							
+								store.index = ch.index
+								return store.index
 	elif (ch.val == 'e'):
 		ch.increment()
 		if (ch.val == 'l'):
@@ -141,7 +189,8 @@ def keyword(index):
 					ch.increment()
 					if (is_space(ch.val) or ch.val == '{'):
 						ch.decrement()
-						return ch.index
+						store.index = ch.index
+						return store.index
 	elif (ch.val == 'f'):
 		ch.increment()
 		if (ch.val == 'o'):
@@ -150,7 +199,8 @@ def keyword(index):
 				ch.increment()
 				if (is_space(ch.val) or ch.val == '('):
 					ch.decrement()
-					return ch.index
+					store.index = ch.index
+					return store.index
 		elif (ch.val == 'l'):
 			ch.increment()
 			if (ch.val == 'o'):
@@ -161,21 +211,24 @@ def keyword(index):
 						ch.increment()
 						if (is_space(ch.val)):
 							ch.decrement()
-							return ch.index			
+							store.index = ch.index
+							return store.index
 	elif (ch.val == 'i'):
 		ch.increment()
 		if (ch.val == 'f'):
 			ch.increment()
 			if (is_space(ch) or ch.val == '('):
 				ch.decrement()
-				return ch.index
+				store.index = ch.index
+				return store.index
 		elif (ch.val == 'n'):
 			ch.increment()
 			if (ch.val == 't'):
 				ch.increment()
 				if (is_space(ch.val)):
 					ch.decrement()
-					return ch.index
+					store.index = ch.index
+					return store.index
 			elif (ch.val == 'c'):
 				ch.increment()
 				if (ch.val == 'l'):
@@ -188,7 +241,8 @@ def keyword(index):
 								ch.increment()
 								if (is_space(ch.val) or ch.val == '>'):
 									ch.decrement()
-									return ch.index
+									store.index = ch.index
+									return store.index
 	elif (ch.val == 'l'):
 		ch.increment()
 		if (ch.val == 'o'):
@@ -199,7 +253,8 @@ def keyword(index):
 					ch.increment()
 					if (is_space(ch.val)):
 						ch.decrement()
-						return ch.index
+						store.index = ch.index
+						return store.index
 	elif (ch.val == 'r'):
 		ch.increment()
 		if (ch.val == 'e'):
@@ -214,7 +269,8 @@ def keyword(index):
 							ch.increment()
 							if (is_space(ch.val)):
 								ch.decrement()
-								return ch.index
+								store.index = ch.index
+								return store.index
 	elif (ch.val == 's'):
 		ch.increment()
 		if (ch.val == 'i'):
@@ -229,7 +285,8 @@ def keyword(index):
 							ch.increment()
 							if (is_space(ch.val) or ch.val == '('):
 								ch.decrement()
-								return ch.index
+								store.index = ch.index
+								return store.index
 		elif (ch.val == 't'):
 			ch.increment()
 			if (ch.val == 'a'):
@@ -242,7 +299,8 @@ def keyword(index):
 							ch.increment()
 							if (is_space(ch.val)):
 								ch.decrement()
-								return ch.index
+								store.index = ch.index
+								return store.index
 		elif (ch.val == 'w'):
 			ch.increment()
 			if (ch.val == 'i'):
@@ -255,7 +313,8 @@ def keyword(index):
 							ch.increment()
 							if (is_space(ch.val)):
 								ch.decrement()
-								return ch.index
+								store.index = ch.index
+								return store.index
 	elif (ch.val == 'v'):
 		ch.increment()
 		if (ch.val == 'o'):
@@ -266,7 +325,8 @@ def keyword(index):
 					ch.increment()
 					if (is_space(ch.val)):
 						ch.decrement()
-						return ch.index
+						store.index = ch.index
+						return store.index
 	elif (ch.val == 'w'):
 		ch.increment()
 		if (ch.val == 'h'):
@@ -279,7 +339,8 @@ def keyword(index):
 						ch.increment()
 						if (is_space(ch.val) or ch.val == '('):
 							ch.decrement()
-							return ch.index
+							store.index = ch.index
+							return store.index	
 	else:
 		return -1
 						
