b5cd8462865eee0289a119e3ca57c5466f6d9ae5
NikhilPr95 <nikhill.prabhu@gmail.com>
2017-04-01 11:08:04 +0530

    Fixed epsilon problem

--- yacc.py
+++ yacc.py
@@ -25,17 +25,18 @@ rules = [
 			"OP_ELSE: else { STATEMENT } ",
 			"ASSIGN: identifier = EXPRESSION ;",			
 			"DECL: type identifier ;",
-			"DEFN: type identifier = EXPRESSION ;",
+			"DEFN: type ASSIGN",
 			"COND: EXPRESSION1 relational_operator EXPRESSION2 ;",
 			"I_ASSIGN: identifier = EXPRESSION",
+			"I_COND: EXPRESSION1 relational_operator EXPRESSION2",
 			"EXPRESSION1: EXPRESSION",
 			"EXPRESSION2: EXPRESSION",
 			"EXPRESSION: E",
 			"E: T E' | T",
-			"E': + T E' | - T E'", # | epsilon",
+			"E': + T E' | + T | - T E' | - T",
 			"T: F T' | F",
-			"T': * F T' | / F T'", # | epsilon",
-			"F: G ^ F | G",
+			"T': * F T' | * F | / F T' | / F",
+			"F: ^ G F | G",
 			"G: ( E ) | identifier | number "
 		 ]
 
@@ -61,17 +62,29 @@ def is_producer(symbol):
 def is_token(symbol):
 	return symbol in token_types
 
-def match_token(token, store, symbol):
+def assign_token_vals(token, node):
+	if token.type in ['number', 'relational_operator']:
+		node.set_synthval(token.val)
+	elif token.type in ['identifier']:
+		node.set_entry(token.val)			
+
+def assign_producer_vals(node):
+	if len(node.children) == 1:
+		child = node.children[0]
+		node.set_synthval(child.synthval)
+		
+def match_token(token, store, symbol, node):
 	print("in match token", token.val, token.type, symbol)
 	if token.type == symbol:
-		store.set_index(token.index + 1)		
+		#assign_token_vals(token, node)
+		store.set_index(token.index + 1)	
 		return True
 	return False
 	
 def is_tuple(symbol):
 	return symbol[0] == '(' and symbol[-1] == ')'
 	
-def is_valid(rule, productions, token, store, stack, root):
+def is_valid(rule, productions, token, store, root):
 	temp = TokenList(token_list,0)
 	update(temp, token)
 	matched = True
@@ -79,14 +92,15 @@ def is_valid(rule, productions, token, store, stack, root):
 	for i in range(len(rule)):
 		symbol = rule[i]
 		child = children[i]
+		print("SYMBOL ", symbol)
 		print("in symbol", symbol, "\t\t(in rule ", rule, ")")			
 		if matched:
 			if is_producer(symbol):
 				print("in producer")
-				if match_rule(temp, store, productions, symbol, stack, child):
+				if match_rule(temp, store, productions, symbol, child):
 					if has_proceeded(temp, store):
 						print("1.SYMBOL in RULE", symbol, rule)
-						if not symbol == rule[-1]:# and rule[rule.index(symbol) + 1] != 'eof':
+						if not symbol == rule[-1]:
 							update(temp, store)
 						print("1. temp store", temp.val, store.val)					
 					else:
@@ -94,20 +108,18 @@ def is_valid(rule, productions, token, store, stack, root):
 						return False
 				else:
 					print("2. unmatched", symbol, temp.val, store.val, rule)
-					#stack.pop()
 					return False
 			elif is_token(symbol):
 				print("is token")
-				if match_token(temp, store, symbol):
+				if match_token(temp, store, symbol, child):
 					if has_proceeded(temp, store):
-						#print("2.SYMBOL in RULE", symbol, rule)
-						if not symbol == rule[-1]:# and rule[rule.index(symbol) + 1] != 'eof':
+						print("2.SYMBOL in RULE", symbol, rule)
+						if not symbol == rule[-1]:
 							update(temp, store)
 						print("matched")
 						print("2. temp store", temp.val, store.val)
 					else:
 						print("3. unmatched not proceeded", symbol, temp.val)
-						#stack.pop()
 						return False
 				else:
 					print("4. unmatched token")
@@ -133,61 +145,36 @@ def is_valid(rule, productions, token, store, stack, root):
 
 def print_tabs(n):
 	print(n*' ', end = " ")
-	
-def print_stack(stack):
-	x=1
-	n = stack.count('(((')
-	i = 0	
-	while i < (len(stack)):
-		print_tabs(n)
-		if not stack[i] == '(((':
-			while (stack[i] != ')))'):
-				print(stack[i], end = "")
-				i += 1
-			n -= 1
-		else:
-			i += 1
 
+	
 def print_tree(root):
-	print(root.val, end= " ")
-	if is_producer(root.val) and root.children:
-		print("(", end = "")
+	print(root.name, end= " ")
+	if is_producer(root.name) and root.children:
+		print("[", end = "")
 	for child in root.children:
 		print_tree(child)
-	if is_producer(root.val) and root.children:
-		print(")", end = " ")
-
-def list_indent_stack(stack):
-	li = []
-	for element in stack:
-		if element == '(((':
-			li.append(list_indent_stack)
+	if is_producer(root.name) and root.children:
+		print("]", end = " ")
 			
-def match_rule(token, store, productions, producer, stack, root):
+def match_rule(token, store, productions, producer, root):
 	print("in match rule with ", producer, ":", productions[producer])
 	for rule in productions[producer]:
 		print("in rule ", rule, " with ", token.val)
-		#stack.append('(((')
-		stack.append(rule)
-		root.set_children(rule)	
+		root.set_children(rule)
+		#assign_producer_vals(root)
 		print("TREE")
 		print_tree(root)
 		print("")
-		print("STACK ", stack)
-		if (is_valid(rule, productions, token, store, stack, root)):
-			print("here we are", rule, productions[producer], productions[producer].index(rule))#, productions[producer][productions[producer].index(rule)])
-			#stack.append(rule)
-		#	stack.append(')))')
+		if (is_valid(rule, productions, token, store, root)):
+			print("here we are", rule, productions[producer], productions[producer].index(rule))
+			#assign_producer_vals(root)
 			return True
 		else:
 			x = None
-			#while(x != '((('):#
-			while(rule != x):
-				x = stack.pop()
-	#			print("Popped ", x," with rule ", rule)
-			#x = stack.pop()
-			#print("Popped ", x," with rule ", rule)
-			print("STACK ", stack)
+			root.delete_children()
+			print("TREE")
+			print_tree(root)
+			print("")
 			print("returned false ", rule, productions[producer])
 	
 	return False
@@ -197,30 +184,21 @@ def start(token_list):
 	token_list.append(eof)
 	token = TokenList(token_list, 0)
 	store = TokenList(token_list,0)
-	stack = []
-
 	root = Tree("PROG")
-	#stack.append('(((')
-	stack.append("PROG")
-	print("TREE")
-	print_tree(root)
-	print("")
-	print("STACK ", stack)
-	if match_rule(token, store, productions, "PROG", stack, root):
-		print("VALID", token.val, store.val)
-	else:
-		print("ERROR", token.val, store.val)
 
 	print("TREE")
 	print_tree(root)
 	print("")
 	
-	return stack
-	#stack.append(')))')
+	if match_rule(token, store, productions, "PROG", root):
+		print("VALID", token.val, store.val)
+	else:
+		print("ERROR", token.val, store.val)
+	
+	return root
 		
 def init_rules():	
 	for rule in rules:
-		#print(rule.split(":"))
 		symbol, prod = rule.split(":")
 		for p in prod.split("|"):
 			add(productions, symbol, p.strip())
@@ -235,9 +213,7 @@ print("\n")
 print("\n")
 
 
-stack = start(token_list)
-
-print("STACK ----")
-print(stack)
-#print_stack(stack)
-#print("ttt",token_types)
+tree = start(token_list)
+print("TREE")
+print_tree(tree)
+print("")
